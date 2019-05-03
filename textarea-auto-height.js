44ddb38623ce26b28a6ce2dd9830dd25867388b7
Vladimir Gerasimenko <vol.gerasimenko@gmail.com>
2019-05-03 14:58:12 +0300

    Update textarea-auto-height.js

--- textarea-auto-height.js
+++ textarea-auto-height.js
@@ -1,11 +1,20 @@
-function addAutoResize() {
-  document.querySelectorAll('[data-autoresize]').forEach(function (element) {
-    element.style.boxSizing = 'border-box';
-    const offset = element.offsetHeight - element.clientHeight;
-    document.addEventListener('input', event => {
-      event.target.style.height = 'auto';
-      event.target.style.height = event.target.scrollHeight + offset + 'px';
-    });
-    element.removeAttribute('data-autoresize');
-  });
+// Example for Vue.js
+// vanila.js: https://stephanwagner.me/auto-resizing-textarea-with-vanilla-javascript
+
+// Usage:
+// - set ref to textarea
+// - set initial textareheight (maybe same as input)
+
+textareaAutoHeight () {
+  if (!this.$refs.textarea) return
+
+  this.$refs.textarea.style.boxSizing = 'border-box'
+  const offset = this.$refs.textarea.offsetHeight - this.$refs.textarea.clientHeight
+  this.$refs.textarea.addEventListener('input', event => {
+    event.target.style.height = this.TEXTAREA_INITIAL_HEIGHT + 'px'
+
+    if (event.target.scrollHeight + offset > this.TEXTAREA_INITIAL_HEIGHT) {
+      event.target.style.height = event.target.scrollHeight + offset + 'px'
+    }
+  })
 }
