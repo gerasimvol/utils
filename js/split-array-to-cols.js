f98169cf095c066d8d26e5a6632021bc89c74556
Vladimir Gerasimenko <vol.gerasimenko@gmail.com>
2020-02-12 14:11:41 +0200

    Update split-array-to-cols.js

--- js/split-array-to-cols.js
+++ js/split-array-to-cols.js
@@ -1,5 +1,5 @@
 import chunk from 'lodash/chunk'
 
 export default function (array, colsAmount) {
-  return chunk(array, Math.round(array.length / colsAmount))
+  return chunk(array, Math.ceil(array.length / colsAmount))
 }
